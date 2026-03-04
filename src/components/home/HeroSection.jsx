4401f4164c286d88d9fefba67c50331ec2e441a4
RajatRawat10 <rajatrawat4242@gmail.com>
2026-03-04 10:19:23 +0530

    landing page

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -4,22 +4,22 @@ import { Leaf, Droplets, Recycle } from "lucide-react";
 const HeroSection = () => {
     return (
         <section
-            className="w-full min-h-screen bg-no-repeat bg-cover bg-center flex items-center pt-[81px]"
+            className="w-full min-h-screen bg-no-repeat bg-cover pt-[81px]"
             style={{
                 backgroundImage: "url('/images/new%201.svg')"
             }}
         >
             <Container>
 
-                <div className="max-w-[700px]  text-primary space-y-8 py-20">
+                <div className="max-w-[700px] flex flex-col justify-left align-left text-accent-foreground space-y-8 py-20">
 
                     {/* Heading */}
-                    <h1 className="font-poppins text-[40px] md:text-[56px] lg:text-[64px] leading-[1.1] font-bold">
-                        Sapsaa: <span className="underline decoration-[#00A8E8] decoration-4 underline-offset-[12px]">The Standard of Pure Water.</span>
+                    <h1 className="font-poppins text-[40px] md:text-[56px] lg:text-[64px] leading-[1.1] font-medium">
+                        Sapsaa: <span className=" decoration-[#00A8E8] decoration-4 underline-offset-[12px]">The Standard of Pure Water.</span>
                     </h1>
 
                     {/* Subheading */}
-                    <p className="text-lg md:text-xl max-w-[550px] font-medium text-primary/80">
+                    <p className="text-lg md:text-xl max-w-[550px] font-normal text-accent-foreground/80">
                         From peak performance to holistic healing—hydration
                         refined by science, perfected by nature.
                     </p>
@@ -42,21 +42,21 @@ const HeroSection = () => {
                             <div className="bg-white text-primary w-14 h-14 flex items-center justify-center rounded-full shadow-lg transition-transform group-hover:scale-110">
                                 <Leaf size={28} />
                             </div>
-                            <p className="font-bold text-lg md:text-xl">EcoFriendly Practices</p>
+                            <p className="font-medium text-lg md:text-xl">EcoFriendly Practices</p>
                         </div>
 
                         <div className="flex items-center gap-4 group">
                             <div className="bg-white text-primary w-14 h-14 flex items-center justify-center rounded-full shadow-lg transition-transform group-hover:scale-110">
                                 <Droplets size={28} />
                             </div>
-                            <p className="font-bold text-lg md:text-xl">Water Conservation</p>
+                            <p className="font-medium text-lg md:text-xl">Water Conservation</p>
                         </div>
 
                         <div className="flex items-center gap-4 group">
                             <div className="bg-white text-primary w-14 h-14 flex items-center justify-center rounded-full shadow-lg transition-transform group-hover:scale-110">
                                 <Recycle size={28} />
                             </div>
-                            <p className="font-bold text-lg md:text-xl">Recycling Initiatives</p>
+                            <p className="font-medium text-lg md:text-xl">Recycling Initiatives</p>
                         </div>
 
                     </div>
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../../components/home/HeroSection";
+import AboutQualitySection from "../../components/home/AboutQualitySection";
 
 const Landing = () => {
     return (
         <div className="page landing">
             <HeroSection />
+            <AboutQualitySection />
         </div>
     );
 };
