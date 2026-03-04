5b1ecd42f6eba7182f2b833bea81bdf490841889
RajatRawat10 <rajatrawat4242@gmail.com>
2026-03-04 12:10:40 +0530

    product review

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,12 @@
 import HeroSection from "../../components/home/HeroSection";
 import AboutQualitySection from "../../components/home/AboutQualitySection";
-
+import ProductsPreview from "../../components/home/ProductsPreview";
 const Landing = () => {
     return (
         <div className="page landing">
             <HeroSection />
             <AboutQualitySection />
+            <ProductsPreview />
         </div>
     );
 };
